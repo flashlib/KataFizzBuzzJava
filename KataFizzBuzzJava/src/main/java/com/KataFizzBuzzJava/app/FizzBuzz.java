24deac67bb261932e91f520f0c5036c12a2c3920
Jacky <flashlib@gmail.com>
2013-12-28 10:30:02 +0800

    Make all tests green.

--- KataFizzBuzzJava/src/main/java/com/KataFizzBuzzJava/app/FizzBuzz.java
+++ KataFizzBuzzJava/src/main/java/com/KataFizzBuzzJava/app/FizzBuzz.java
@@ -7,7 +7,7 @@ public class FizzBuzz {
     public static String say(int number) {
         if (number == 5)
             return "Buzz";
-        if (number == 3)
+        if (number%3 == 0)
             return "Fizz";
         if (number == 2)
             return "2";
